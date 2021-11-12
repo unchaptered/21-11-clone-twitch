6c101734c373ce2b6301592875e78772924a55b9
Unchaptered <workstation19961002@gmail.com>
2021-11-12 21:05:15 +0900

    Modify SVGIcons
    Add new props about isBig(size)

--- src/Components/SVGIcon.js
+++ src/Components/SVGIcon.js
@@ -4,6 +4,11 @@ const SVG = styled.svg`
   margin: 0 5px;
   padding: 5.5px;
   width: 30px;
+  width: ${({ isBig }) => (isBig === false ? "30px" : "45px")};
+  transition: opacity 0.2s ease-in;
+  &:hover {
+    opacity: 50%;
+  }
 `;
 
 export default ({
@@ -14,9 +19,16 @@ export default ({
   fillRule,
   clipRule,
   d: dArr = ["d"],
+  isBig = false,
 }) => {
   return (
-    <SVG xmlns={xmlns} className={className} viewBox={viewBox} fill={fill}>
+    <SVG
+      xmlns={xmlns}
+      className={className}
+      viewBox={viewBox}
+      fill={fill}
+      isBig={isBig}
+    >
       {dArr.length === 1 ? (
         <path d={dArr[0]} fillRule={fillRule} clipRule={clipRule} />
       ) : (
